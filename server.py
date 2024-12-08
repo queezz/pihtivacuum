49f0057b698e188ab63957c5447bf85dd7b826de
ak <queezz@gmail.com>
2024-12-08 15:48:10 +0900

    relative server for front-back communication

--- server.py
+++ server.py
@@ -56,4 +56,4 @@ def get_state():
     return jsonify(elements_state)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
--- static/script.js
+++ static/script.js
@@ -31,7 +31,7 @@ function toggleElementStatus(element, colors, confirmToggle) {
     console.log(`${element.id} fill changed to: ${newFill}`);
 
     // Send the update to the backend
-    fetch('http://127.0.0.1:5000/update', {
+    fetch('/update', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
